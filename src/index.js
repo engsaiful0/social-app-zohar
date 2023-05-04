30ba328d94e67f567627e5a7e2ccd5a4bfcf79e8
saiful islam <engsaiful0@gmail.com>
2023-05-04 09:14:16 +0600

    4/5/2023

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, { StrictMode} from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+//router
+import {  createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//store
+
+import { Provider } from 'react-redux';
+//reducer
+import {store} from './store'
+
+import { IndexRouters } from './router';
+import { SimpleRouter } from './router/simple-router';
+import { ChatRouter } from './router/chat-router';
+
+const router = createBrowserRouter([
+
+  ...IndexRouters,
+  ...SimpleRouter,
+  ...ChatRouter
+], { basename: process.env.PUBLIC_URL })
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+    <App>
+      <RouterProvider router={router}>
+      </RouterProvider>
+      </App>
+    </Provider>
+  </StrictMode>
+)
+
---
+++ src/router/simple-router.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+// auth
+import ConfirmMail from "../views/dashboard/auth/confirm-mail";
+import LockScreen from "../views/dashboard/auth/lock-screen";
+import Recoverpw from "../views/dashboard/auth/recoverpw";
+import SignIn from "../views/dashboard/auth/sign-in";
+import SignUp from "../views/dashboard/auth/sign-up";
+import ForgotPassword from "../views/dashboard/auth/forgot-password";
+// errors
+import Error404 from "../views/dashboard/errors/error404";
+import Error500 from "../views/dashboard/errors/error500";
+
+//extrpages
+import Maintenance from "../views/dashboard/extrapages/maintenance";
+import ComingSoon from "../views/dashboard/extrapages/comingsoon";
+
+export const SimpleRouter = [
+  {
+    path: "auth/confirm-mail",
+    element: <ConfirmMail />,
+  },
+  {
+    path: "auth/lock-screen",
+    element: <LockScreen />,
+  },
+  {
+    path: "auth/recoverpw",
+    element: <Recoverpw />,
+  },
+  {
+    path: "auth/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "auth/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "auth/forgot-password",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "errors/error404",
+    element: <Error404 />,
+  },
+  {
+    path: "errors/error500",
+    element: <Error500 />,
+  },
+  {
+    path: "extra-pages/pages-maintenance",
+    element: <Maintenance />,
+  },
+  {
+    path: "extra-pages/pages-comingsoon",
+    element: <ComingSoon />,
+  },
+];
